09235ebfb6d49654948b1636e31ca3c6d353ab47
Vitaliy Ogurtsov <vetalreal@ukr.net>
2013-12-14 18:04:38 +0200

    jQuery+jQueryUI.

--- js/jq1.js
+++ js/jq1.js
@@ -20,15 +20,43 @@
 //    $('#right-panel').toggleClass('down');
 //  });
 //});
-$('.box-header').click(function() {
-  $(this).next('.box-content').fadeToggle();
+//$('.box-header').click(function() {
+//  $(this).next('.box-content').slideToggle();
+//});
+//$(document).on('click','.box-header',function() {
+//  $(this).next('.box-content').slideToggle();
+//});
+$('.box').each(function() {
+  $(this).attr('start_width', $(this).width());
+  console.log('0');
+});
+$('.box-header').toggle(function() {
+  $(this).parent().animate({'width': '30px'}, 400, 'easeOutBack');
+  console.log('1');
+}, function() {
+  var start_width = $(this).parent().attr('start_width');
+  $(this).parent().animate({'width': start_width + 'px'}, 600, 'easeOutBack');
+  console.log('2');
 });
 $("form").submit(function(event) {
   $(this).find('.requare').each(function() {
     if ($(this).next().val() == '') {
       event.preventDefault();
-      $(this).next().val('Ведите значение').css({'color':'red'});
+      $(this).next().val('Ведите значение').css({'color': 'red'});
       $(this).css({'background-color': 'red', 'color': 'white'});
     }
   });
 });
+$('#right-panel>div').click(function() {
+  $('.box-header').trigger('click');
+});
+
+$('#curr-page').attr('box-count', $('#sidebar>div.box').length - 1);
+$('#curr-page').click(function() {
+  $('#sidebar>div.box:first-child').clone().appendTo('#sidebar');
+  var new_box_count = +($('#curr-page').attr('box-count')) + 1;
+  //var new_box_count = 1+parseInt($('#curr-page').attr('box-count'));
+  $('#curr-page').attr('box-count', new_box_count);
+});
+$("#content").resizable();
+$('.box').draggable();
